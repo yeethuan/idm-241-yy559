89824837f510880253326025ba5099f989945988
Yi <yy559@drexel.edu>
2024-10-15 11:29:04 -0400

    push

--- idm241/alpha/backup.js
+++ idm241/alpha/backup.js
@@ -1,3 +1,35 @@
+
+// EMAIL SNIPPET WORD LENGTHS
+const originalText = document.querySelector('.snippet').innerText;
+
+function truncateSnippet(selector, wordLimit) {
+    const element = document.querySelector(selector);
+    const words = originalText.split(' '); 
+
+    if (words.length > wordLimit) {
+        const truncatedText = words.slice(0, wordLimit).join(' ') + '...';
+        element.innerText = truncatedText;
+    } else {
+        element.innerText = originalText; 
+    }
+}
+
+function applySnippetTruncation() {
+    const mediaQuery = window.matchMedia('(max-width: 1500px)');
+
+    if (mediaQuery.matches) {
+        truncateSnippet('.snippet', 18);  
+    } else {
+        truncateSnippet('.snippet', 20);  
+    }
+}
+
+applySnippetTruncation();
+
+window.addEventListener('resize', applySnippetTruncation);
+
+
+
 // TRASH
 let trashCount = 0;
 let lastDeletedEmail = null;
