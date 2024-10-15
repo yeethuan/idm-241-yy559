a67fe1940973f05484020680c9133513eaee33eb
Yi <yy559@drexel.edu>
2024-10-14 22:01:43 -0400

    everything

--- idm241/alpha/build.js
+++ idm241/alpha/build.js
@@ -1,12 +1,33 @@
+
+
+
+
+
+// EMAIL SNIPPET WORD LENGTHS
+const originalText = document.querySelector('.snippet').innerText;
+
 function truncateSnippet(selector, wordLimit) {
     const element = document.querySelector(selector);
-    const text = element.innerText;
-    const words = text.split(' ');
+    const words = originalText.split(' '); 
 
     if (words.length > wordLimit) {
         const truncatedText = words.slice(0, wordLimit).join(' ') + '...';
         element.innerText = truncatedText;
+    } else {
+        element.innerText = originalText; 
     }
 }
 
-truncateSnippet('.snippet', 26);
+function applySnippetTruncation() {
+    const mediaQuery = window.matchMedia('(max-width: 1500px)');
+
+    if (mediaQuery.matches) {
+        truncateSnippet('.snippet', 18);  
+    } else {
+        truncateSnippet('.snippet', 26);  
+    }
+}
+
+applySnippetTruncation();
+
+window.addEventListener('resize', applySnippetTruncation);
